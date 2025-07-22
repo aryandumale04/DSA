7dc9a23111863cce188c61f4d73cc799da215c77
aryandumale04 <aryandumale04@gmail.com>
2025-07-22 16:45:46 +0530

    Add Fibonacci in SelfPractice

--- SelfStudy/fibonacci.cpp
+++ SelfStudy/fibonacci.cpp
@@ -1,43 +1,23 @@
-#include<iostream>
-using namespace std;
-void Fibonaci(int n){
-
-    if(n<=0){
-        return ;
-    }
-    else{
-        long long a=0;
-        long long b=1;
-        cout<< a << " ";
-        long long  next=0;
-        
-        for(int i =1;i<n;i++){
-            cout << b <<" ";
-            next=a+b;
-            
-            a=b;
-            b=next;
-
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0;
+        int b = 1;
+        if( n==0 ){
+            return a;
         }
+        if( n==1 ){
+            return b;
+        }
+        int prev1 = 0;
+        int prev2 = 1;
+        int curr = 0;
+        for(int i = 2; i <= n; i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return curr;
+        
     }
-
-
-}
-int main(){
-    int n;
-    cout<<"Fibonacci Series Program"<<endl;
-    cout<<"Enter the terms in fibonacci series : ";
-
-    cin>>n;
-
-    Fibonaci(n);
-   
-
-
-
-
-
-    
-
-    
-}
+};
