91145ba52cc276d59f957226f1533702c20d255c
Aryan Dumale <aryandumale04@gmail.com>
2026-02-03 22:34:30 +0530

    Update Ransom Note

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,22 @@
-#include<iostream>
-using namespace std ; 
+#include<bits/stdc++.h>
+using namespace std;
+
 int main() {
-    cout<< "Jojo ko maaro ";
+    int n ;
+    cin >> n;
+    cin.ignore();
+    int x;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+       int x ;
+       cin >> x;
+       arr[i] = x;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+        if (i != n - 1) cout<<" ";
+    }
+
     return 0;
 }
