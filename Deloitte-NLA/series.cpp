dcebc9aca9520aba319358d1f2e99a3c83c048b5
Aryan Dumale <aryandumale04@gmail.com>
2026-02-04 15:09:22 +0530

    Add series.cpp

---
+++ Deloitte-NLA/series.cpp
@@ -0,0 +1,20 @@
+int main () {
+    long long number;
+    long long  series;
+    long long sum = 0;
+    cin >> number >> series;
+
+    // processing logic
+    for (int i = 1; i <= series; i++) {
+        int j = i;
+        int num = 0;
+        while(j > 0) {
+            num = (num * 10) + number; 
+            j--;
+        }
+        sum += num;
+    }
+
+    cout << sum;
+    return 0;
+}
