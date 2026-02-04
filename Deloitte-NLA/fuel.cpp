af0fe167b981efef49fa2e77a96b0ff316073b29
Aryan Dumale <aryandumale04@gmail.com>
2026-02-04 15:09:00 +0530

    Add Deloitte- NLA

---
+++ Deloitte-NLA/fuel.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    long long fuelNeeded;
+    long long cost;
+    long long amt;
+    cin >> fuelNeeded >> cost;
+    amt = fuelNeeded * cost;
+    cout << amt << "\n";
+    return 0;
+}
