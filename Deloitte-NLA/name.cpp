db80caa1dc56e35fef4c4b6eee89a4b7175729e6
Aryan Dumale <aryandumale04@gmail.com>
2026-02-07 21:29:05 +0530

    Update Longest Consecutive sequence

---
+++ Deloitte-NLA/name.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    string inp;
+    getline(cin, inp);
+
+    string answer = "";
+    
+    int n = inp.length();
+    for (int i = 0; i < n; i++) {
+        if (i < n - 1 && inp[i+1] == '-') {
+            // need to store current character;
+            answer += inp[i];
+        }
+        if (i == n - 1) {
+            answer += inp[i];
+        }
+    }
+    cout<<answer;
+
+    return 0;
+}
