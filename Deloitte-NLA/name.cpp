d38b9d34a1e50c368e70b7ae51588f9f279de04c
Aryan Dumale <aryandumale04@gmail.com>
2026-02-13 21:07:26 +0530

    Update name

--- Deloitte-NLA/name.cpp
+++ Deloitte-NLA/name.cpp
@@ -7,8 +7,8 @@ int main () {
     getline(cin, inp);
 
     string answer = "";
-    
     int n = inp.length();
+
     for (int i = 0; i < n; i++) {
         if (i < n - 1 && inp[i+1] == '-') {
             // need to store current character;
@@ -18,7 +18,7 @@ int main () {
             answer += inp[i];
         }
     }
-    cout<<answer;
 
+    cout<<answer;
     return 0;
 }
