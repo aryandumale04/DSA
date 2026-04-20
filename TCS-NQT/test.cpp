2a6f79682d540cca6e2879c51acccf7da6bce411
Aryan Dumale <aryandumale04@gmail.com>
2026-04-20 15:17:50 +0530

    Add TCS-NQT

---
+++ TCS-NQT/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std; 
+
+int main () {
+
+    // ULTRA BASIC
+    // int x;
+    // string a;
+    // cin >> x;
+    // getline(cin, a);
+
+    // cout<< x << endl;
+    // cout << a << endl;
+    return 0;
+}
