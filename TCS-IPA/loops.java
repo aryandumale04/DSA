58005c0a63517229543229c821671fe20c14c988
Aryan Dumale <aryandumale04@gmail.com>
2026-06-22 12:10:39 +0530

    Add loops

---
+++ TCS-IPA/loops.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        // System.out.print("Enter your age : ");
+        // int age = sc.nextInt();
+
+        // if (age % 3 == 0) {
+        //     System.out.println(age + " is divisible by 3");
+        // } else {
+        //     System.out.println(age + " is not divisible by 3");
+        // }
+
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
