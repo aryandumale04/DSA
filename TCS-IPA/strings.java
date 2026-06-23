fb59f7624802e1ef5ff87102cd2c7153f20c2eb3
Aryan Dumale <aryandumale04@gmail.com>
2026-06-23 11:32:03 +0530

    Move arrays, strings

---
+++ TCS-IPA/strings.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(name.length() - 1));
+
+
+
+
+
+    }
+}
